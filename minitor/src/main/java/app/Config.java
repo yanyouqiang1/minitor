b12b521a7fa2b0fe984e1c16a285da5b2a219cee
yanyouqiang1 <374580618@qq.com>
2017-07-04 23:50:03 +0800

    监控服务器 数据处理

--- minitor/src/main/java/app/Config.java
+++ minitor/src/main/java/app/Config.java
@@ -3,9 +3,19 @@ package app;
 import app.handle.commonHandle.warehouse.HistoryDataListener;
 import app.handle.commonHandle.warehouse.MsgWarehouseImpl;
 import app.handle.commonHandle.warehouse.WarehoseInter;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.UnknownHostException;
+
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * Created by Administrator on 2017/7/4 0004.
@@ -20,5 +30,25 @@ public class Config {
         MsgWarehouseImpl msgWarehouse = new MsgWarehouseImpl();
         msgWarehouse.setHistoryDataListener(historyDataListener);
         return msgWarehouse;
+    }
+
+    @Bean
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory)
+            throws UnknownHostException {
+        RedisTemplate<Object, Object> template = new RedisTemplate<Object, Object>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        template.setValueSerializer(jackson2JsonRedisSerializer); //1
+        template.setKeySerializer(new StringRedisSerializer()); //2
+
+        template.afterPropertiesSet();
+        return template;
     }
 }
