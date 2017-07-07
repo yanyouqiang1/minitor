54088cd366abb58213ed174a1fab2d0c87ea554f
yanyouqiang1 <374580618@qq.com>
2017-07-07 10:35:10 +0800

    统计属性改变

--- minitor/src/main/java/app/handleconfig/handler/OverallHandler.java
+++ minitor/src/main/java/app/handleconfig/handler/OverallHandler.java
@@ -33,16 +33,28 @@ public class OverallHandler implements OverallStatisticsHandler {
 
         //rate
         long visitors = overallStatistics.getVisitors();
-        overall.setRate_method_delete((float) (overallStatistics.getMethod_delete()/visitors));
-        overall.setRate_method_get((float) (overallStatistics.getMethod_get()/visitors));
-        overall.setRate_method_post((float) (overallStatistics.getMethod_post()/visitors));
-        overall.setRate_method_put((float) (overallStatistics.getMethod_put()/visitors));
-        overall.setRate_status_100((float) (overallStatistics.getStatus_100()/visitors));
-        overall.setRate_status_200((float) (overallStatistics.getStatus_200()/visitors));
-        overall.setRate_status_300((float) (overallStatistics.getStatus_300()/visitors));
-        overall.setRate_status_400((float) (overallStatistics.getStatus_400()/visitors));
-        overall.setRate_status_500((float) (overallStatistics.getStatus_500()/visitors));
+        if(visitors==0) {
+            overall.setRate_method_delete(0f);
+            overall.setRate_method_get(0f);
+            overall.setRate_method_post(0f);
+            overall.setRate_method_put(0f);
+            overall.setRate_status_100(0f);
+            overall.setRate_status_200(0f);
+            overall.setRate_status_300(0f);
+            overall.setRate_status_400(0f);
+            overall.setRate_status_500(0f);
+        }else{
+            overall.setRate_method_delete((float) (overallStatistics.getMethod_delete() / visitors));
+            overall.setRate_method_get((float) (overallStatistics.getMethod_get() / visitors));
+            overall.setRate_method_post((float) (overallStatistics.getMethod_post() / visitors));
+            overall.setRate_method_put((float) (overallStatistics.getMethod_put() / visitors));
+            overall.setRate_status_100((float) (overallStatistics.getStatus_100() / visitors));
+            overall.setRate_status_200((float) (overallStatistics.getStatus_200() / visitors));
+            overall.setRate_status_300((float) (overallStatistics.getStatus_300() / visitors));
+            overall.setRate_status_400((float) (overallStatistics.getStatus_400() / visitors));
+            overall.setRate_status_500((float) (overallStatistics.getStatus_500() / visitors));
 
+        }
         overall.setTPS(overallStatistics.getTPS());
         keeptoSave.setOverall(overall);
     }
---
+++ minitor/src/test/java/test1.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/7/7.
+ */
+public class test1 {
+    public static void main(String[] args){  
+        float f = 21f;
+    }
+    
+}
